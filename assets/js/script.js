fca1f5e1802cc3f3b920cbe454738ac3c1082943
Maulana Chandra Irawan <irawan@it.student.pens.ac.id>
2024-10-30 16:23:52 +0000

    homeprofil3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
+// dropdown
 function toggleDropdown() {
   const dropdown = document.getElementById('servicesDropdown');
   dropdown.classList.toggle('show');
 }
 
-function showTab(tabId) {
-  // Hapus class active dari semua tab dan tab panel
-  document.querySelectorAll(".tab-button").forEach((button) => button.classList.remove("active"));
-  document.querySelectorAll(".tab-panel").forEach((panel) => panel.classList.remove("active"));
+// profil-daerah
+function showContent(section, buttonId) {
+  document.getElementById('sejarah').classList.add('hidden');
+  document.getElementById('geografi').classList.add('hidden');
+  document.getElementById('demografi').classList.add('hidden');
+  document.getElementById(section).classList.remove('hidden');
 
-  // Tambahkan class active pada tab dan panel yang dipilih
-  document.getElementById("tab-" + tabId).classList.add("active");
-  document.getElementById("content-" + tabId).classList.add("active");
+  document.getElementById('btn-sejarah').classList.remove('active');
+  document.getElementById('btn-geografi').classList.remove('active');
+  document.getElementById('btn-demografi').classList.remove('active');
+  document.getElementById(buttonId).classList.add('active');
 }
