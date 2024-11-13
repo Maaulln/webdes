22f85f3f56ac5bc1f894c9cc4832b52be329895b
Maulana Chandra Irawan <irawan@it.student.pens.ac.id>
2024-11-13 14:32:08 +0000

    third

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
-// dropdown
+function toggleNavbar() {
+  const navbar = document.getElementById('navbar');
+  navbar.classList.toggle('hidden'); // Toggles the hidden class
+}
+
 function toggleDropdown() {
   const dropdown = document.getElementById('servicesDropdown');
-  dropdown.classList.toggle('show');
+  dropdown.classList.toggle('show'); // Toggles the show class
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+        const hamburgerButton = document.getElementById('hamburgerButton');
+        const mobileMenu = document.getElementById('mobileMenu');
+
+        // Event listener untuk klik tombol hamburger
+        hamburgerButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+    });
 
 // profil-daerah
 function showContent(section, buttonId) {
@@ -16,3 +30,20 @@ function showContent(section, buttonId) {
   document.getElementById('btn-demografi').classList.remove('active');
   document.getElementById(buttonId).classList.add('active');
 }
+
+// Mendapatkan elemen header
+        const header = document.getElementById('header');
+
+        // Fungsi untuk mengubah warna header berdasarkan scroll
+        window.addEventListener('scroll', () => {
+            const homeSection = document.getElementById('home');
+            const homeSectionBottom = homeSection.offsetHeight;
+
+            if (window.scrollY > homeSectionBottom) {
+                header.classList.remove('bg-white', 'text-black');
+                header.classList.add('bg-gray-800', 'text-white');
+            } else {
+                header.classList.remove('bg-gray-800', 'text-white');
+                header.classList.add('bg-white', 'text-black');
+            }
+        });
